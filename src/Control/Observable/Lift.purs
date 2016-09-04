eaf4a847402be76e1ede84ae107b52afaf462ae1
justinwoo <moomoowoo@gmail.com>
2016-09-04 18:11:17 +0200

    add liftAff

--- src/Control/Observable/Lift.purs
+++ src/Control/Observable/Lift.purs
@@ -1,13 +1,17 @@
 module Control.Observable.Lift
   ( liftCallback
+  , liftAff
   ) where
 
 import Prelude
+import Control.Monad.Aff (cancel, Aff, runAff)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Exception (error)
 import Control.Observable (Observable, OBSERVABLE, free, observable)
 
+
 -- | Lift a callback function into an Observable.
-liftCallback :: forall a e b.
+liftCallback :: forall e a b.
   (
     (a -> Eff (observable :: OBSERVABLE | e) Unit) ->
     Eff (observable :: OBSERVABLE | e) b
@@ -16,3 +20,9 @@ liftCallback :: forall a e b.
 liftCallback source = observable \sink -> do
   source sink.next
   free []
+
+-- | Lift an Aff into an Observable.
+liftAff :: forall e a. Aff (observable :: OBSERVABLE | e) a -> Eff (observable :: OBSERVABLE | e) (Observable a)
+liftAff aff = observable \sink -> do
+    canceler <- runAff sink.error sink.next aff
+    free [{unsubscribe: void $ runAff (const $ pure unit) (const $ pure unit) (canceler `cancel` (error "Unsubscribed"))}]
